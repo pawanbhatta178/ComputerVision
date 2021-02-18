e99f810e012d44cbb126c39c71d8b482d34a9ca0
pawan <pawanbhatta178@gmail.com>
2021-02-18 13:34:02 -0500

    Project 2 IO done

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -8,7 +8,7 @@ int toInt(string input)
     return stoi(input);
 }
 
-class imageProcessing
+class ImageProcessing
 {
 public:
     int numRows, numCols, minVal, maxVal, newMin, newMax, thrVal;
@@ -23,7 +23,7 @@ public:
 
     void threshold(int *arr1, int *arr2, int frameSize) {}
 
-    void imgReformat(int *inAry, imageProcessing *outImg, int frameSize)
+    void imgReformat(int *inAry, ImageProcessing *outImg, int frameSize)
     {
     }
 
@@ -54,15 +54,19 @@ public:
     }
     int avg3x3(int i, int j)
     {
+        return 0;
     }
     int median3x3(int i, int j)
     {
+        return 0;
     }
     int CP5x5(int i, int j)
     {
+        return 0;
     }
     int convolution()
     {
+        return 0;
     }
     void aryToFile(int **ary, ofstream &outFile, int frameSize)
     {
@@ -75,13 +79,14 @@ public:
 int main(int argc, const char *argv[])
 {
     //READ
-    string inputName = argv[1]; //(1) get the input file
-    ifstream input;             //(2) instantiate the input
-    input.open(inputName);      //(3) tell the input object
+    string inputName = argv[1];
+    ifstream input;
+    input.open(inputName);
+
     int thrVal = toInt(argv[2]);
 
     //WRITES
-    string rfImgName{argv[3]}, avgOutImgName{argv[4]}, avgThrImgName{argv[5]}, avgPrettyPrintName{argv[6]}, medianOutImgName{argv[7]}, medianThrImgName{argv[8]}, medianPrettyPrintName{argv[9]}, CPOutImgName{argv[10]}, CPThrImgName{argv[11]}, CPPrettyPrintName{argv[12]};
+    string rfImgName = argv[3], avgOutImgName = argv[4], avgThrImgName = argv[5], avgPrettyPrintName = argv[6], medianOutImgName = argv[7], medianThrImgName = argv[8], medianPrettyPrintName = argv[9], CPOutImgName = argv[10], CPThrImgName = argv[11], CPPrettyPrintName = argv[12];
     ofstream rfImg, AvgOutImg, AvgThrImg, AvgPrettyPrint, MedianOutImg, MedianThrImg, MedianPrettyPrint, CPOutImg, CPThrImg, CPPrettyPrint;
     rfImg.open(rfImgName);
     AvgOutImg.open(avgOutImgName);
@@ -99,7 +104,10 @@ int main(int argc, const char *argv[])
 
         if (rfImg.is_open() && AvgOutImg.is_open() && AvgThrImg.is_open() && AvgPrettyPrint.is_open() && MedianOutImg.is_open() && MedianThrImg.is_open() && MedianPrettyPrint.is_open() && CPOutImg.is_open() && CPThrImg.is_open() && CPPrettyPrint.is_open())
         {
-        }
+            ImageProcessing imgProcessing;
+            imgProcessing.thrVal = thrVal;
+            input >> imgProcessing.numRows >> imgProcessing.numCols >> imgProcessing.minVal >> imgProcessing.maxVal;
+                }
         else
         {
             cout << "Error: Some output files is missing or couldnt be opened" << endl;
