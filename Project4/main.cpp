0d6f1ede8274a5da93f786508b6fa0c264412a16
pawan <pawanbhatta178@gmail.com>
2021-03-19 17:44:12 -0400

    class members set up

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -3,10 +3,40 @@
 #include <string>
 using namespace std;
 
+struct Property
+{
+    int label;     // The component label
+    int numPixels; // total number of pixels in the cc.
+    int minR;
+    int minC;
+    int maxR;
+    int maxC;
+    // In the Cartesian coordinate system, any rectangular box can be
+    //represented by two points: upper-left corner and the lower -right
+    //corner of the box. Here, the two points:(minR minC) and(maxR maxC)
+    //represents the smallest rectangular box that the cc can fit inside the box.
+};
+
 class CClabel
 {
-
 public:
+    int numRows;
+    int numCols;
+    int minVal;
+    int maxVal;
+    int newMin;
+    int newMax;
+    int newLabel;  // initialize to 0
+    int trueNumCC; // the true number of connected components in the image
+    // It will be determined in manageEQAry method.
+    int **zeroFramedAry;       // a 2D array, need to dynamically allocate
+    int NonZeroNeighborAry[5]; // 5 is the max number of neighbors you have to check.
+    // For easy programming, you may consider using this 1-D array
+    // to store pixel (i, j)’s non-zero neighbors during pass 1 and pass2.
+    int *EQAry; // an 1-D array, of size (numRows * numCols) / 4
+    // dynamically allocate at run time
+    // and initialize to its index, i.e., EQAry[i] = i.
+    Property *CCproperty;
 };
 
 int main(int argc, const char *argv[])
@@ -29,6 +59,7 @@ int main(int argc, const char *argv[])
     {
         if (rfPrettyPrint.is_open() && labelFile.is_open() && propertyFile.is_open())
         {
+            labelFile << "HEY";
         }
         else
         {
@@ -39,4 +70,10 @@ int main(int argc, const char *argv[])
     {
         cout << "ERROR: The input file with following name does not exists or there was problem reading it: " << inputName << endl;
     }
+
+    input.close();
+    rfPrettyPrint.close();
+    labelFile.close();
+    propertyFile.close();
+    return 0;
 }
